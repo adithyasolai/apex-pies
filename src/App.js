99d66a24640674b269c5869562bc2dcc18271e17
Adithya Solai <ppalanic@terpmail.umd.edu>
2022-04-08 18:00:08 -0400

    added logo header to front-end

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { Component } from "react";
+import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import apex_logo from "../resources/Apex_Logo_Final.png";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <header>
+            <Link to="/">
+              <img src={apex_logo} alt="asdf"/>
+            </Link>
+          </header>
+
+          {/* Determines which page to serve. */}
+          {/* <Switch>
+            <Route path="/result/:age">
+              <Details />
+            </Route>
+
+            <Route path="/">
+              <SearchParams />
+            </Route>
+          </Switch> */}
+        </Router>
+
+      </div>
+    )
+    
+  }
+}
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
