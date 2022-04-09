2906a38ade1516281a52c7469486e9bc0f53da36
Adithya Solai <ppalanic@terpmail.umd.edu>
2022-04-09 01:20:22 -0400

    created User Form, sent user input to python file via flask

---
+++ src/UserForm.js
@@ -0,0 +1,127 @@
+import { Component } from "react";
+
+const SECTORS = ["Tech", "Health", "Energy", "Banking"];
+
+class UserForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      age: 18, // lowest possible age to invest is 18
+      risk: 1, // ranges from 1-10
+      sector: "" // no sector selected at the beginning.
+    };
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(JSON.stringify(this.state));
+
+    fetch('http://localhost:5000/', {
+      method: 'POST',
+      headers : {
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify(this.state)
+    }).then(response => { console.log(response.json()) });
+
+    fetch('http://localhost:5000/',{
+      method: 'GET',
+      headers : {
+        'Content-Type':'application/json'
+      }
+    }).then(response => { console.log(response.json()) });
+  }
+
+  render() {
+    console.log("Current Age Value: ", this.state.age); 
+    console.log("Current Risk Tolerance Value: ", this.state.risk); 
+    console.log("Current Sector Selected: ", this.state.sector); 
+    return (
+      <div className="userForm">
+        <form
+          onSubmit={this.handleSubmit.bind(this)}
+        >
+          {/* missing htmlFor */}
+          <label> 
+            Age: 
+            <select
+              value={this.state.age}
+              onChange={(e) => this.setState({age: e.target.value})} 
+              onBlur={(e) => this.setState({age: e.target.value})} // this is just here for accessibility for disabled people that use screenreaders.
+            >
+              {/* empty option */}
+              <option/> 
+
+              {/* 18-74 */}
+              {
+                Array.from(Array(57),(x,i)=>i+18).map((ageNum) => (
+                  <option value={ageNum} key={ageNum}>
+                    {ageNum}
+                  </option>
+                ))
+              }
+
+              {/* 75 internally, but displayed as "75+" */}
+              <option value={75}>
+                75+
+              </option>
+
+            </select>
+          </label>
+
+          <label> 
+            Risk Tolerance: 
+            <select
+              value={this.state.risk}
+              onChange={(e) => this.setState({risk: e.target.value})} 
+              onBlur={(e) => this.setState({risk: e.target.value})} // this is just here for accessibility for disabled people that use screenreaders.
+            >
+              {/* empty option */}
+              <option/> 
+
+              {/* 1-10 */}
+              {
+                Array.from(Array(10),(x,i)=>i+1).map((riskNum) => (
+                  <option value={riskNum} key={riskNum}>
+                    {riskNum}
+                  </option>
+                ))
+              }
+
+            </select>
+          </label>
+
+          <label>
+            Sector of Interest
+            <select
+              value={this.state.sector}
+              onChange={(e) => this.setState({sector: e.target.value}) } 
+              onBlur={(e) => this.setState({sector: e.target.value})} // this is just here for accessibility for disabled people that use screenreaders.
+            >
+              {/* empty option */}
+              <option/> 
+
+              {/* All sectors */}
+              {
+                SECTORS.map((s) => (
+                  <option value={s} key={s}>
+                    {s}
+                  </option>
+                ))
+              }
+
+            </select>
+
+          </label>
+
+          <button>Submit</button>
+        </form>
+
+      </div>
+    )
+   
+  }
+}
+
+export default UserForm;
