de079e8be10c83abb3e24dcadd284158161ef62c
Adithya Solai <ppalanic@terpmail.umd.edu>
2022-04-09 20:53:42 -0400

    whitespace fixes

--- src/PieResults.js
+++ src/PieResults.js
@@ -1,33 +1,31 @@
 // import { useParams } from 'react-router-dom';
 // import { useLocation } from 'react-router-dom'
-import { Component } from 'react';
+import { Component } from "react";
 import { withRouter } from "react-router-dom";
-
 
 class PieResults extends Component {
   constructor() {
     super();
 
-    this.state = {loading: true};
+    this.state = { loading: true };
   }
 
   async componentDidMount() {
     const response = await fetch(
-      fetch('http://localhost:5000/fetchpies', {
-      method: 'POST',
-      headers : {
-        'Content-Type':'application/json'
-      },
-      body: JSON.stringify({userId: this.props.location.state.userId})
-    })
+      fetch("http://localhost:5000/fetchpies", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ userId: this.props.location.state.userId }),
+      })
     );
 
-    console.log(response)
+    console.log(response);
 
     // only make loading false after getting the data from the API
-    this.setState({loading: false});
+    this.setState({ loading: false });
   }
-
 
   render() {
     // console.log(this.props);
@@ -38,9 +36,11 @@ class PieResults extends Component {
 
     return (
       <div>
-        <h1>Age: {age} -- Risk: {risk} -- Sector: {sector} -- User ID: {userId}</h1>
+        <h1>
+          Age: {age} -- Risk: {risk} -- Sector: {sector} -- User ID: {userId}
+        </h1>
       </div>
-    )
+    );
   }
 }
 
--- src/UserForm.js
+++ src/UserForm.js
@@ -1,154 +1,151 @@
-  import { Component } from "react";
-  import { withRouter } from "react-router-dom";
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-  const SECTORS = ["Tech", "Health", "Energy", "Banking"];
+const SECTORS = ["Tech", "Health", "Energy", "Banking"];
 
-  class UserForm extends Component {
-    constructor() {
-      super();
+class UserForm extends Component {
+  constructor() {
+    super();
 
-      this.state = {
-        age: 18, // lowest possible age to invest is 18
-        risk: 1, // ranges from 1-10
-        sector: "", // no sector selected at the beginning.
-        userId: "DummyUser" // filled in later and sent to PieResults page to fetch pies from BackEnd
-      };
-    }
-
-    handleSubmit(event) {
-      event.preventDefault();
-      console.log(JSON.stringify(this.state));
-
-      fetch('http://localhost:5000/', {
-        method: 'POST',
-        headers : {
-          'Content-Type':'application/json'
-        },
-        body: JSON.stringify(this.state)
-      })
-
-      this.props.history.push({
-        pathname: '/pieresults',
-        state: this.state
-      })
-
-    }
-
-    render() {
-      console.log("Current Age Value: ", this.state.age); 
-      console.log("Current Risk Tolerance Value: ", this.state.risk); 
-      console.log("Current Sector Selected: ", this.state.sector); 
-      return (
-        <div className="userForm">
-          <form
-            onSubmit={this.handleSubmit.bind(this)}
-          >
-            {/* missing htmlFor */}
-            <html>
-            <head>
-            </head>
-            <body>
-
-            </body>
-            </html>
-            <label > 
-            <div class="myDIV">Age (Hover above me for more info!): </div>
-            <div class="hide">Generally speaking, the younger an investor is, the riskier their portfolio can be. This is due to the fact they probably will not be withdrawing their money for years and therefore have much more time to recover and recoup from any losses. The higher the risk of a portfolio, the higher its beta. </div>
-              <select
-                value={this.state.age}
-                onChange={(e) => this.setState({age: e.target.value})} 
-                onBlur={(e) => this.setState({age: e.target.value})} // this is just here for accessibility for disabled people that use screenreaders.
-              >
-                {/* empty option */}
-                <option/> 
-
-                {/* 18-74 */}
-                {
-                  Array.from(Array(57),(x,i)=>i+18).map((ageNum) => (
-                    <option value={ageNum} key={ageNum}>
-                      {ageNum}
-                    </option>
-                  ))
-                }
-
-                {/* 75 internally, but displayed as "75+" */}
-                <option value={75}>
-                  75+
-                </option>
-
-              </select>
-            </label>
-
-            <br/>
-            <br/>
-            <label> 
-            <div class="myDIV">Risk Tolerance (Hover above me for more info!): </div> 
-            <div class="hide">The amount of risk an investor takes on depends on several other factors besides age. An investor should also take into account their existing debt, savings account balance, and net worth. An investor with low debt combined with high savings account balance and net worth would have a higher risk tolerance. 
-</div>
-              <select
-                value={this.state.risk}
-                onChange={(e) => this.setState({risk: e.target.value})} 
-                onBlur={(e) => this.setState({risk: e.target.value})} // this is just here for accessibility for disabled people that use screenreaders.
-              >
-                {/* empty option */}
-                <option/> 
-
-                {/* 1-10 */}
-                {
-                  Array.from(Array(10),(x,i)=>i+1).map((riskNum) => (
-                    <option value={riskNum} key={riskNum}>
-                      {riskNum}
-                    </option>
-                  ))
-                }
-
-              </select>
-            </label>
-
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-
-            <label>
-            <div class="myDIV"> Sector of Interest (Hover above me for more info!): </div> 
-            <div class="hide">Sectors are very different from each other in terms of risk and return. In general, when compared to Tech and Energy, Banking and Healthcare tend to be less riskier sectors, which means that they typically have a lower return.  
-</div>
-              <select
-                value={this.state.sector}
-                onChange={(e) => this.setState({sector: e.target.value}) } 
-                onBlur={(e) => this.setState({sector: e.target.value})} // this is just here for accessibility for disabled people that use screenreaders.
-              >
-                {/* empty option */}
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-
-                All sectors
-                {
-                  SECTORS.map((s) => (
-                    <option value={s} key={s}>
-                      {s}
-                    </option>
-                  ))
-                }
-
-              </select>
-
-            </label>
-            
-
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <button>Submit</button>
-          </form>
-
-        </div>
-      )
-    
-    }
+    this.state = {
+      age: 18, // lowest possible age to invest is 18
+      risk: 1, // ranges from 1-10
+      sector: "", // no sector selected at the beginning.
+      userId: "DummyUser", // filled in later and sent to PieResults page to fetch pies from BackEnd
+    };
   }
 
-  const UserFormWithRouter = withRouter(UserForm);
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(JSON.stringify(this.state));
 
-  export default UserFormWithRouter;
+    fetch("http://localhost:5000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.state),
+    });
+
+    this.props.history.push({
+      pathname: "/pieresults",
+      state: this.state,
+    });
+  }
+
+  render() {
+    console.log("Current Age Value: ", this.state.age);
+    console.log("Current Risk Tolerance Value: ", this.state.risk);
+    console.log("Current Sector Selected: ", this.state.sector);
+    return (
+      <div className="userForm">
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          {/* missing htmlFor */}
+          <label>
+            <div className="myDIV">Age (Hover above me for more info!): </div>
+            <div className="hide">
+              Generally speaking, the younger an investor is, the riskier their
+              portfolio can be. This is due to the fact they probably will not
+              be withdrawing their money for years and therefore have much more
+              time to recover and recoup from any losses. The higher the risk of
+              a portfolio, the higher its beta.{" "}
+            </div>
+            <select
+              value={this.state.age}
+              onChange={(e) => this.setState({ age: e.target.value })}
+              onBlur={(e) => this.setState({ age: e.target.value })} // this is just here for accessibility for disabled people that use screenreaders.
+            >
+              {/* empty option */}
+              <option />
+
+              {/* 18-74 */}
+              {Array.from(Array(57), (x, i) => i + 18).map((ageNum) => (
+                <option value={ageNum} key={ageNum}>
+                  {ageNum}
+                </option>
+              ))}
+
+              {/* 75 internally, but displayed as "75+" */}
+              <option value={75}>75+</option>
+            </select>
+          </label>
+
+          <br />
+          <br />
+          <label>
+            <div className="myDIV">
+              Risk Tolerance (Hover above me for more info!):{" "}
+            </div>
+            <div className="hide">
+              The amount of risk an investor takes on depends on several other
+              factors besides age. An investor should also take into account
+              their existing debt, savings account balance, and net worth. An
+              investor with low debt combined with high savings account balance
+              and net worth would have a higher risk tolerance.
+            </div>
+            <select
+              value={this.state.risk}
+              onChange={(e) => this.setState({ risk: e.target.value })}
+              onBlur={(e) => this.setState({ risk: e.target.value })} // this is just here for accessibility for disabled people that use screenreaders.
+            >
+              {/* empty option */}
+              <option />
+
+              {/* 1-10 */}
+              {Array.from(Array(10), (x, i) => i + 1).map((riskNum) => (
+                <option value={riskNum} key={riskNum}>
+                  {riskNum}
+                </option>
+              ))}
+            </select>
+          </label>
+
+          <br />
+          <br />
+          <br />
+          <br />
+
+          <label>
+            <div className="myDIV">
+              {" "}
+              Sector of Interest (Hover above me for more info!):{" "}
+            </div>
+            <div className="hide">
+              Sectors are very different from each other in terms of risk and
+              return. In general, when compared to Tech and Energy, Banking and
+              Healthcare tend to be less riskier sectors, which means that they
+              typically have a lower return.
+            </div>
+            <select
+              value={this.state.sector}
+              onChange={(e) => this.setState({ sector: e.target.value })}
+              onBlur={(e) => this.setState({ sector: e.target.value })} // this is just here for accessibility for disabled people that use screenreaders.
+            >
+              {/* empty option */}
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1"
+              />
+              All sectors
+              {SECTORS.map((s) => (
+                <option value={s} key={s}>
+                  {s}
+                </option>
+              ))}
+            </select>
+          </label>
+
+          <br />
+          <br />
+          <br />
+          <br />
+          <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const UserFormWithRouter = withRouter(UserForm);
+
+export default UserFormWithRouter;
